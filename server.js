f90dc8689c21ff6afdaa6e8c16dbe93761de8c6a
isahiljha <sahil.aptron1@gmail.com>
2025-03-22 14:41:16 +0530

    second try

--- server.js
+++ server.js
@@ -1,31 +1,26 @@
-// Import the necessary modules
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRouter = require("./routes/auth/auth-routes");
-const adminProductsRouter = require("./routes/admin/products-routes");
-const adminOrderRouter = require("./routes/admin/order-routes");
+const authRouter = require("../routes/auth/auth-routes");
+const adminProductsRouter = require("../routes/admin/products-routes");
+const adminOrderRouter = require("../routes/admin/order-routes");
+const shopProductsRouter = require("../routes/shop/products-routes");
+const shopCartRouter = require("../routes/shop/cart-routes");
+const shopAddressRouter = require("../routes/shop/address-routes");
+const shopOrderRouter = require("../routes/shop/order-routes");
+const shopSearchRouter = require("../routes/shop/search-routes");
+const shopReviewRouter = require("../routes/shop/review-routes");
+const commonFeatureRouter = require("../routes/common/feature-routes");
 
-const shopProductsRouter = require("./routes/shop/products-routes");
-const shopCartRouter = require("./routes/shop/cart-routes");
-const shopAddressRouter = require("./routes/shop/address-routes");
-const shopOrderRouter = require("./routes/shop/order-routes");
-const shopSearchRouter = require("./routes/shop/search-routes");
-const shopReviewRouter = require("./routes/shop/review-routes");
+const app = express();
 
-const commonFeatureRouter = require("./routes/common/feature-routes");
-
-// Connect to MongoDB
 mongoose
   .connect(process.env.DBURL)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
 
-const app = express();
-
-// Middleware
 app.use(
   cors({
     origin: process.env.FRONTEND_URL,
@@ -44,19 +39,15 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 
-// API Routes
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
-
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
-
 app.use("/api/common/feature", commonFeatureRouter);
 
-// Export the serverless handler (for Vercel)
-module.exports = app;
+module.exports = app; // Export the app for use with Vercel's serverless function
