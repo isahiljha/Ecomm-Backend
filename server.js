45d7904f65c8b5a9ffc16e86d38427b5b0bc7434
isahiljha <sahil.aptron1@gmail.com>
2025-03-22 14:54:15 +0530

    final try

--- server.js
+++ server.js
@@ -3,23 +3,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const authRouter = require("../routes/auth/auth-routes");
-const adminProductsRouter = require("../routes/admin/products-routes");
-const adminOrderRouter = require("../routes/admin/order-routes");
-const shopProductsRouter = require("../routes/shop/products-routes");
-const shopCartRouter = require("../routes/shop/cart-routes");
-const shopAddressRouter = require("../routes/shop/address-routes");
-const shopOrderRouter = require("../routes/shop/order-routes");
-const shopSearchRouter = require("../routes/shop/search-routes");
-const shopReviewRouter = require("../routes/shop/review-routes");
-const commonFeatureRouter = require("../routes/common/feature-routes");
+const authRouter = require("./routes/auth/auth-routes");
+const adminProductsRouter = require("./routes/admin/products-routes");
+const adminOrderRouter = require("./routes/admin/order-routes");
 
-const app = express();
+const shopProductsRouter = require("./routes/shop/products-routes");
+const shopCartRouter = require("./routes/shop/cart-routes");
+const shopAddressRouter = require("./routes/shop/address-routes");
+const shopOrderRouter = require("./routes/shop/order-routes");
+const shopSearchRouter = require("./routes/shop/search-routes");
+const shopReviewRouter = require("./routes/shop/review-routes");
+
+const commonFeatureRouter = require("./routes/common/feature-routes");
+
 
 mongoose
-  .connect(process.env.DBURL)
+  .connect(process.env.DBURL,)
   .then(() => console.log("MongoDB connected"))
   .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+
 
 app.use(
   cors({
@@ -38,16 +44,17 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
 app.use("/api/shop/review", shopReviewRouter);
+
 app.use("/api/common/feature", commonFeatureRouter);
 
-module.exports = app; // Export the app for use with Vercel's serverless function
+app.listen(PORT, () => console.log(`Server is now running on port ${PORT}`));
